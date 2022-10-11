407575b0733fa216660d20750ec1c53636c549d0
PCDIEGO\diego <druibals@gmail.com>
2022-10-11 13:38:38 +0200

    Project Upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    Animator animator;
+    public float speed = 10;
+    public float rotationSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if(vertical > 0)
+        {
+            rigidbody.AddForce(transform.up * vertical * speed * Time.deltaTime);
+            animator.SetBool("MoveBoolean", true);
+        }
+        else
+        {
+            animator.SetBool("MoveBoolean", false);
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.eulerAngles = transform.eulerAngles - new Vector3(0, 0, horizontal * rotationSpeed * Time.deltaTime);
+
+    }
+}
